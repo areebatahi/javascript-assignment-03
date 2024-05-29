eb2e5d6445f5b6c849d29e59a89b1d0675b935c3
areebatahi <areebatahirarain@gmail.com>
2024-05-29 21:44:41 +0500

    messge

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //1. 
-var StudentName = ``;
+var StudentName = [];
 
 // 2. 
-var objectNatation = {
-
-};
+var objectNatation = new Array();
 
 // 3. 
 var strings = ["Areeba", "Barira", "Fatima", "Amna", "Muskan", "Summaya"];
@@ -41,33 +39,37 @@ document.write(
 );
 
 // 9.
-// var colorName = [" Red ", " Yellow ", " Blue "];
+var colorName = [" Red ", " Yellow ", " Blue "];
 // document.write(colorName + "<br>");
 
-// colorName[0] = prompt("Enter any colour name to display in start");
-// document.write(colorName + "<br>");
+// var userColor = prompt("Enter any colour name to display in start");
+// colorName.unshift(userColor)
+// document.write(colorName);
 
-// var userColor = prompt("Enter any colour name to display in end");
-// colorName.push(userColor);
-// document.write(colorName + "<br>");
+// var userColorInEnd = prompt("Enter any colour name to display in end");
+// colorName.push(userColorInEnd);
+// document.write(colorName);
 
-// colorName.push("Gold", "Voilet");
-// document.write(colorName + "<br>");
+// colorName.unshift("Gold", "Voilet");
+// document.write(colorName);
 
 // colorName.shift();
-// document.write(colorName + "<br>");
+// document.write(colorName);
 
 // colorName.pop();
-// document.write(colorName + "<br>");
+// document.write(colorName);
 
 // var userColorIndex = prompt("Enter position of color name");
 // colorName[userColorIndex] = prompt("Enter any colour name");
-// document.write(colorName + "<br>");
+// document.write(colorName);
 
 // var deleteIndex = +prompt("At which index you wants to delete a color");
 // var deleteNumbr = +prompt("how many colors you want to delete");
-// colors.splice(deleteIndex,deleteNumbr);
-// document.write(colors+ "<br><br><br>");
+// colorName.splice(deleteIndex,deleteNumbr);
+// document.write(colorName);
+
+// document.write("<br>");
+// document.write("<br>");
 
 // 10.
 var numbers = [320, 230, 480, 120];
@@ -77,7 +79,11 @@ document.write(
 
 
 // 11. 
-var cityName = ["Karachi", "Islamabad", "Lahore", "Qutta", "Multan", "Sukkar", "Peshawar"]
+var cities = ['karachi', 'lahore', 'quetta', 'peshawar', 'naran', 'kashmir'];
+document.write("Cities list  :<br>", cities, "<br>");
+
+var selectedCities = cities.slice(2, 5);
+document.write("selected cities<br> ", selectedCities, "<br><br><br>");
 
 
 // 12.
@@ -87,19 +93,26 @@ document.write(
     "<h1> Arrays :</h1>" + arr + "<h1>Strings  :</h1>" + arrayJoin
 );
 
-// 13.
-var device = ["Keyboard", "Mouse", "Printer", "Moniter"];
-document.write(
-    "<h1>Device: </h1>" + device + "<br>" + "<h3>Out: </h3>" + device[0] + "<h3>Out: </h3>" + device[1] + "<h3>Out: </h3>" + device[2] + "<h3>Out: </h3>" + device[3]
-);
+//13
+var fifo = ['keyboard', 'mouse', 'printer', 'monitor'];
+var returns = fifo.slice();
+document.write("<b>Devices</b><br>", returns, "<br><br>");
 
-// 14.
-document.write(
-    "<h1>Device: </h1>" + device + "<br>" + "<h3>Out: </h3>" + device[3] + "<h3>Out: </h3>" + device[2] + "<h3>Out: </h3>" + device[1] + "<h3>Out: </h3>" + device[0]
-);
+//14
+var lifo = ['keyboard', 'mouse', 'printer', 'monitor'];
+var returns = lifo.reverse()
+document.write("<b>Devices</b><br>", returns, "<br><br>");
 
 // 15.
 var phoneManufacture = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
 document.write(
-    "<h1> phone manufacturers </h1> " + "<ol>" + "<li>" + phoneManufacture[0] + " </li>" + "<li>" + phoneManufacture[1] + "</li>" + "<li>" + phoneManufacture[2] + "</li>" + "<li>" + phoneManufacture[3] + "</li>" + "<li>" + phoneManufacture[4] + "</li>" + "<li>" + phoneManufacture[5] + "</li>" + "</ol>"
+    "<h1> phone manufacturers </h1> " +
+    "<select>" +
+    '<option value="' + phoneManufacture[0] + '">' + phoneManufacture[0] + '</option>' +
+    '<option value="' + phoneManufacture[1] + '">' + phoneManufacture[1] + '</option>' +
+    '<option value="' + phoneManufacture[2] + '">' + phoneManufacture[2] + '</option>' +
+    '<option value="' + phoneManufacture[3] + '">' + phoneManufacture[3] + '</option>' +
+    '<option value="' + phoneManufacture[4] + '">' + phoneManufacture[4] + '</option>' +
+    '<option value="' + phoneManufacture[5] + '">' + phoneManufacture[5] + '</option>' +
+    "</select>"
 );
